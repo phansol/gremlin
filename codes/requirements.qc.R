39eaccea10d7fe612a7ff987c28669cf87c49577
Hansol Park <hs0530@kaist.ac.kr>
2021-09-13 19:17:40 +0900

    Delete codes directory

--- codes/requirements.qc.R
+++
@@ -1,3 +0,0 @@
-if (!suppressWarnings(require("rpart"))){install.packages("rpart", repos = "http://cran.r-project.org")}
-if (!requireNamespace("BiocManager", quietly = TRUE)){install.packages("BiocManager")}
-if (!suppressWarnings(require("GWASTools"))){BiocManager::install("GWASTools")}
